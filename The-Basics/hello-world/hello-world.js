b4b08e66a3e4aa9097d57e8bc331fea1a868195a
infernezor@gmail.com <infernezor@gmail.com>
2021-03-17 17:16:55 -0400

    comment on hello world for viewing.

--- The-Basics/hello-world/hello-world.js
+++ The-Basics/hello-world/hello-world.js
@@ -1,7 +1,13 @@
 function helloWorld() {
-    const hello = "Hello, World!"
+    const hello = "Hello, World!"; // defines a string variable named hello.
+    return hello; // returns the string.
+}
+
+function main(){
+    let hello = "Placeholder until we call our 'helloWorld' function!";
+    hello = helloWorld();
     return hello;
 }
 
-module.exports = helloWorld;
+module.exports = main;
 
