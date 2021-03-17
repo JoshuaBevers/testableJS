7738cb35fe4f1de0ced6133215351593eec7a465
infernezor@gmail.com <infernezor@gmail.com>
2021-03-17 18:47:18 -0400

    changed toBe.

--- The-Basics/hello-world/Hello-World.spec.js
+++ The-Basics/hello-world/Hello-World.spec.js
@@ -1,8 +1,8 @@
+const { test, expect } = require('@jest/globals');
 const helloWorld = require('./hello-world');
 
 test("Print 'Hello, world'", () => {
-    const helloWorldString = helloWorld();
-    // const helloWorldString = 'Hello, World!';
-    expect(helloWorldString).toEqual('Hello, World!');
-
-})
+  const helloWorldString = helloWorld();
+  // const helloWorldString = 'Hello, World!';
+  expect(helloWorldString).toBe('Hello, World!');
+});
